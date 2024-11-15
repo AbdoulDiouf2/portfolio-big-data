ad924acde1ae5d307aed0d92e4bd3ec871c2aa5b
Abdoul Ahad Mbacke DIOUF <148540282+AbdoulDiouf2@users.noreply.github.com>
2024-11-15 22:37:23 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
       <nav className="container mx-auto px-4 py-3">
         <div className="flex items-center justify-between">
           <a href="#home" className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-            AMD
+            AAMD
           </a>
           
           {/* Navigation Bureau */}
